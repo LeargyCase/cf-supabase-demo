e3eb97e1a286312408c43054ab138cc44feb5e0c
LeargyCase <1657909025@qq.com>
2025-05-23 16:17:39 +0800

    chore: fully remove cf-supabase-demo submodule references

--- cf-supabase-demo/src/services/index.ts
+++
@@ -1,9 +0,0 @@
-import CacheService from './CacheService';
-import NotificationService from './NotificationService';
-import DataService from './DataService';
-
-export {
-  CacheService,
-  NotificationService,
-  DataService
-}; 
